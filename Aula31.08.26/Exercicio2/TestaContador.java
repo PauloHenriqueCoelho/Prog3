7f8b9864fc468bfc476910bfe98887e651753a81
PauloHenriqueCoelho <paulocoelhogonca@gmail.com>
2026-09-15 19:38:25 -0300

    Delete Aula31.08.26 directory

--- Aula31.08.26/Exercicio2/TestaContador.java
+++
@@ -1,9 +0,0 @@
-public class TestaContador {
-    public static void main(String[] args) {
-        Contador c1 = new Contador();
-        Contador c2 = new Contador();
-        Contador c3 = new Contador();
-
-        Contador.mostrarTotal();
-    }
-}
